426139c80640aa4afba04d6a7a20241634e0e849
Md. Khairul Islam <khairulislamtanim@gmail.com>
2022-07-28 21:34:14 -0400

    Add recent codes and updated plotter (#38)

--- TFT-pytorch/script/utils.py
+++ TFT-pytorch/script/utils.py
@@ -14,11 +14,11 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, mean_square
 def calculate_result(y_true, y_pred):
     mae = mean_absolute_error(y_true, y_pred)
     rmse = np.sqrt(mean_squared_error(y_true, y_pred))
-    msle = mean_squared_log_error(y_true, y_pred)
+    # msle = mean_squared_log_error(y_true, y_pred)
     smape = symmetric_mean_absolute_percentage(y_true, y_pred)
     nnse = normalized_nash_sutcliffe_efficiency(y_true, y_pred)
 
-    return mae, rmse, msle, smape, nnse
+    return mae, rmse, smape, nnse
 
 def remove_outliers(original_df, use_median=True, multiplier=7.5, verbose=False):
     df = original_df.copy()
@@ -94,6 +94,16 @@ def symmetric_mean_absolute_percentage(y_true, y_pred):
     value = np.where(np.isnan(value), 0, value)
     
     return np.mean(value)
+
+def show_result(df: pd.DataFrame, targets:List[str]):    
+    for target in targets:
+        predicted_column = f'Predicted_{target}'
+        y_true, y_pred = df[target].values, df[predicted_column].values
+
+        mae, rmse, smape, nnse = calculate_result(y_true, y_pred)
+        print(f'Target {target}, MAE {mae:.5g}, RMSE {rmse:.5g}, SMAPE {smape:0.5g}. NNSE {nnse:0.5g}.')
+    print()
+
 
 def get_start_dates(parameters: Parameters):
     max_encoder_length = parameters.model_parameters.input_sequence_length
@@ -183,11 +193,7 @@ def train_test_split(
 def scale_data(
         train_data:DataFrame, validation_data:DataFrame, test_data:DataFrame, parameters:Parameters
     ):
-    train, test = train_data.copy(), test_data.copy()
-    if validation_data is None:
-        validation = None
-    else:
-        validation = validation_data.copy()
+    train, validation, test = train_data.copy(), validation_data.copy(), test_data.copy()
 
     if parameters.preprocess.scale_input:
         scaled_features = parameters.data.static_features + parameters.data.dynamic_features
